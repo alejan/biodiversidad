1be839e4d9c811cdb766a106c9ad473c6681b436
Felipe <notclevermail@gmail.com>
2017-02-07 23:53:02 -0500

    ajustar recurso estaticos

--- catalogo/settings.py
+++ catalogo/settings.py
@@ -120,12 +120,12 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'avatar')
 STATIC_URL = '/avatar/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'avatar'),
+
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
