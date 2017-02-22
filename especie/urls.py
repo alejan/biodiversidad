5fc71d5834eef5bebe6ade200447ea6c30026a45
felipe.martinez <notclevermail@gmail.com>
2017-02-21 18:15:19 -0600

    corregir error de urls

--- especie/urls.py
+++ especie/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.listado_especies, name='index'),
-    url(r'^(?P<nombre_categoria>\w+)/$', views.listado_especies, name='por_categorias'),
+    url(r'^categoria/(?P<nombre_categoria>\w+)/$', views.listado_especies, name='por_categorias'),
     url(r'^detalle/(?P<id_especie>\d+)/', views.detalle_especie, name='detalle'),
     url(r'^registro/$', views.registro, name='registro'),
     url(r'^perfil/$', views.perfil, name='perfil'),
